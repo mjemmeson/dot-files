071577feb0d42e78da00bd7a3bbb0a07be87e98e
Michael Jemmeson <michael.jemmeson@cpan.org>
2013-05-29 13:09:55 +0100

    scpmj function

--- custom_links/work_desktop/.bash_work_desktop.sh
+++ custom_links/work_desktop/.bash_work_desktop.sh
@@ -2,6 +2,11 @@
 
 alias sshmj="ssh mjemmeson@ec2-54-242-208-227.compute-1.amazonaws.com"
 
+scpmj()
+{
+    scp "$1" mjemmeson@ec2-54-242-208-227.compute-1.amazonaws.com:
+}
+
 #eval $(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)
 
 if [ -f $HOME/perl5/perlbrew/etc/bashrc ]; then
