e4191b89356186ae8b7f905dbbe47a6039309665
Michael Jemmeson <mjemmeson@cpan.org>
2014-11-26 15:56:14 +0000

    removing old aws link

--- custom_links/home/.bash_home.sh
+++ custom_links/home/.bash_home.sh
@@ -1,7 +1,5 @@
 # home.sh
 
-alias sshmj="ssh mjemmeson@ec2-54-242-208-227.compute-1.amazonaws.com"
-
 #eval $(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)
 
 if [ -f $HOME/perl5/perlbrew/etc/bashrc ]; then
